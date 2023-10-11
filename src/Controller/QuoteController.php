bb4a5b1a0e6952208d5dd27ce251948dbcb75916
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-10-11 12:59:06 +0200

    form can select movies

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -4,22 +4,32 @@ namespace App\Controller;
 
 use App\Entity\MovieQuote;
 use App\Form\Type\MovieQuoteType;
-use Doctrine\DBAL\Types\TextType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuoteController extends AbstractController
 {
     #[Route('/create-quote', name: 'create_quote')]
-    public function createQuote(EntityManagerInterface $entityManager): Response
+    public function createQuote(Request $request): Response
     {
         $movieQuote = new MovieQuote();
-        $movieQuote->setQuote("test");
-        $movieQuote->setCharacter("Daniel Pillwein");
 
         $form = $this->createForm(MovieQuoteType::class, $movieQuote);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $movieQuote = $form->getData();
+            echo $movieQuote->getQuote();
+            echo $movieQuote->getMovie()->getName();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }
 
         return $this->render('quote/create_quote.html.twig', [
             'form' => $form,
--- src/Form/Type/MovieQuoteType.php
+++ src/Form/Type/MovieQuoteType.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Form\Type;
 
+use App\Entity\Movie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,6 +17,11 @@ class MovieQuoteType extends AbstractType
         $builder
             ->add('quote', TextType::class)
             ->add('character', TextType::class)
+            ->add('movie', EntityType::class, [
+                'class' => Movie::class,
+                'choice_label' => 'name',
+                'choice_value' => 'id'
+            ])
             ->add('save', SubmitType::class)
         ;
     }
