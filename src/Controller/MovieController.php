44de300d283e90ad91f281a9d293518863e5e345
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-10-11 13:43:14 +0200

    creating movies working

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Movie;
+use App\Form\Type\MovieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    #[Route('/create-movie', name: 'create_movie')]
+    public function createQuote(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $movie = new Movie();
+
+        $form = $this->createForm(MovieType::class, $movie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $movie = $form->getData();
+
+            $entityManager->persist($movie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('all_quotes');
+        }
+
+        return $this->render('movie/create_movie.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
