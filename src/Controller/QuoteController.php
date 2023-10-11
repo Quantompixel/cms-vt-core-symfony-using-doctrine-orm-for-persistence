e48aaccb1a083fecd231d3179a2eb107ebe6425a
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-10-11 12:24:17 +0200

    form renders

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\MovieQuote;
+use App\Form\Type\MovieQuoteType;
+use Doctrine\DBAL\Types\TextType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuoteController extends AbstractController
 {
-    #[Route('/add-new', name: 'quote_add')]
-    public function add(): Response
+    #[Route('/create-quote', name: 'create_quote')]
+    public function createQuote(EntityManagerInterface $entityManager): Response
     {
-       return new Response("test");
+        $movieQuote = new MovieQuote();
+        $movieQuote->setQuote("test");
+        $movieQuote->setCharacter("Daniel Pillwein");
+
+        $form = $this->createForm(MovieQuoteType::class, $movieQuote);
+
+        return $this->render('quote/create_quote.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
