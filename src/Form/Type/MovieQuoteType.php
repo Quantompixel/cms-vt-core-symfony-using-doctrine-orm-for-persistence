86990a4b85b976c61d1b9d1d7c62bb147b7f3246
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2024-01-17 13:13:27 +0100

    translated forms

--- src/Form/Type/MovieQuoteType.php
+++ src/Form/Type/MovieQuoteType.php
@@ -9,20 +9,30 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\MovieQuote;
+use Symfony\Component\Translation\Translator;
 
 class MovieQuoteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $translator = new Translator('');
+
         $builder
-            ->add('quote', TextType::class)
-            ->add('character', TextType::class)
+            ->add('quote', TextType::class, [
+                'label' => $translator->trans('Quote')
+            ])
+            ->add('character', TextType::class, [
+                'label' => $translator->trans('Character')
+            ])
             ->add('movie', EntityType::class, [
+                'label' => $translator->trans('Movie'),
                 'class' => Movie::class,
                 'choice_label' => 'name',
                 'choice_value' => 'id'
             ])
-            ->add('save', SubmitType::class)
+            ->add('save', SubmitType::class, [
+                'label' => $translator->trans('Save'),
+            ])
         ;
     }
 
--- src/Form/Type/MovieType.php
+++ src/Form/Type/MovieType.php
@@ -9,15 +9,24 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\Translator;
 
 class MovieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $translator = new Translator('');
+
         $builder
-            ->add('name', TextType::class)
-            ->add('releaseYear', NumberType::class)
-            ->add('save', SubmitType::class)
+            ->add('name', TextType::class, [
+                'label' => $translator->trans('Name')
+            ])
+            ->add('releaseYear', NumberType::class, [
+                'label' => $translator->trans('Release Year')
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => $translator->trans('Save')
+            ])
         ;
     }
 
