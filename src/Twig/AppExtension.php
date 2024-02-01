f959eb6288cfd141a8850b54368a55a5fc0f72dd
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2024-02-01 08:00:23 +0100

    extending twig finished

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,6 +4,8 @@ namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig\TwigTest;
 
 class AppExtension extends AbstractExtension
 {
@@ -13,6 +15,36 @@ class AppExtension extends AbstractExtension
             new TwigFilter('quote', [$this, 'formatQuote']),
             new TwigFilter('characterName', [$this, 'formatCharacterName']),
         ];
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('year', [$this, 'calculateYears'])
+        ];
+    }
+
+    public function getTests()
+    {
+        return [
+            new TwigTest('wizany', [$this, 'isMarkus'])
+        ];
+    }
+
+    public function isMarkus(string $value): bool
+    {
+        return $value === 'Markus Wizany';
+    }
+
+    public function calculateYears(int $year)
+    {
+        $timespan = getdate(time())['year'] - $year;
+
+        if ($timespan === 0) {
+            return 'this year';
+        }
+
+        return $timespan . ' years ago';
     }
 
     public function formatQuote(string $quote): string
